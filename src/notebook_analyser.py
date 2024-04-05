b0d9e2833d9a661d4bff466d8d14546ca9f4d5c8
Naziah Siddique <naziah.siddique@riotinto.com>
2024-04-05 15:56:08 +1000

    added exit if notebooks contain functions. edited docstrings

--- src/notebook_analyser.py
+++ src/notebook_analyser.py
@@ -1,10 +1,9 @@
-import ast
-import glob
-
-import json
+"""Scirpt to check notebook files for function definitions."""
 
 import argparse
-
+import ast
+import glob
+import json
 from typing import List
 
 
@@ -24,9 +23,9 @@ def has_function(code: str) -> bool:
             return True
     return False
 
+
 def analyze_notebooks(notebook_paths: List[str]) -> bool:
-    """Iterates over all the notebooks provided to it and checks whether any of the cells
-    within that notebook has a function or not.
+    """Iterates over provided notebook paths and checks whether any notebook code cells contain function definitions.
 
     Args:
         notebook_paths (List[str]): A list of notebook file paths
@@ -38,28 +37,42 @@ def analyze_notebooks(notebook_paths: List[str]) -> bool:
     for notebook in notebook_paths:
         with open(notebook, "r") as notebook_file:
             notebook_contents = json.load(notebook_file)
-            
+
             has_function_flag = False
             for cell_id, cell in enumerate(notebook_contents["cells"]):
                 if cell["cell_type"] == "code":
                     source_code = "".join(cell["source"])
                     if has_function(source_code):
-                        print(f"notebook_analyser: {notebook} has function in cell number {cell_id}")
+                        print(
+                            f"notebook_analyser: {notebook} has function in cell number {cell_id}"
+                        )
                         has_function_flag = True
                         raise_error = True
 
             if not has_function_flag:
-                print(f"notebook_analyser: {notebook} has no functions in it :)")
-    
+                print(
+                    f"notebook_analyser: {notebook} has no functions in it :)"
+                )
+
     return raise_error
+
 
 if __name__ == "__main__":
     # Initialize parser
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-n", "--nb-directory", help="Source directory of notebooks to analyze.", required=True)
+    parser.add_argument(
+        "-n",
+        "--nb-directory",
+        help="Source directory of notebooks to analyze.",
+        required=True,
+    )
     args = parser.parse_args()
 
-    notebook_paths = glob.glob(f"{args.nb_directory}/**/*.ipynb", recursive=True)
-    print(notebook_paths)
-    analyze_notebooks(notebook_paths)
+    notebook_paths = glob.glob(
+        f"{args.nb_directory}/**/*.ipynb", recursive=True
+    )
+
+    contains_functions = analyze_notebooks(notebook_paths)
+    if contains_functions:
+        exit(1)
